e4fce24c8248835d518f94f57661a268cda971b1
John van de Wetering <wetering@cs.ru.nl>
2019-03-19 14:00:49 +0100

    Made publications searchable by keyword

--- generate_html.py
+++ generate_html.py
@@ -1,5 +1,12 @@
 import bibtexparser
 from bibtexparser.bwriter import BibTexWriter
+from bibtexparser.bibdatabase import BibDatabase
+
+writer = BibTexWriter()
+writer.indent = '    '
+writer.display_order = ('ENTRYTYPE', 'author', 'title', 'year', 'journal', 'booktitle',
+                        'editor', 'series', 'volume', 'pages', 'publisher', 'doi', 'link',
+                        'keyword',  'keywords', 'abstract')
 
 def entry_sort_key(entry):
     l = entry['link']
@@ -12,10 +19,18 @@ def normalise_name(n):
     p1, p2 = n.split(', ')
     return p2.strip() + " " + p1.strip()
 
-HTML = r"""<a target="_blank" href="{url}">{title}</a>, {authors}, <i>{journal}</i> ({year}). <br><p class="abstract"><b>Abstract:</b> {abstract}</p>"""
+HTML = r"""<a target="_blank" href="{url}">{title}</a>, {authors}, <i>{journal}</i> ({year}).<br>
+Keywords: {keywords}.&nbsp;&nbsp;
+<span class="abstract">{abstract}</span>&nbsp;&nbsp;
+<span class="bibdata"><pre><code>{bibdata}</code></pre></span>"""
 
+keyword_pubs = dict()
 
 def entry_to_html(entry):
+    db = BibDatabase()
+    db.entries = [entry]
+    raw_bibdata = writer.write(db)
+    
     e = bibtexparser.customization.author(entry)
     bibtexparser.customization.convert_to_unicode(entry)
     authors = ""
@@ -27,17 +42,33 @@ def entry_to_html(entry):
             authors += normalise_name(a) + ", "
         authors += normalise_name(e['author'][-2]) + " and "
         authors += normalise_name(e['author'][-1])
+
     if 'journal' in entry:
         journal = entry['journal']
     elif 'booktitle' in entry:
         journal = entry['booktitle']
     else:
         journal = entry['note']
-
     journal = journal.replace('\n', ' ')
-    return HTML.format(url = entry['link'], title=entry['title'],
+
+    if 'keyword' in entry:
+        keywords = [s.strip() for s in entry['keyword'].split(',')]
+    else: keywords = []
+    keyword_html = ", ".join('<a target="_blank" href="{kw}.html">{kw}</a>'.format(kw=kw) for kw in keywords)
+    html = HTML.format(url = entry['link'], title=entry['title'],
                        authors = authors, journal = journal,
-                       year = entry['year'], abstract=entry['abstract'])
+                       year = entry['year'], abstract=entry['abstract'],
+                       bibdata=raw_bibdata, keywords = keyword_html)
+    for kw in keywords:
+        if kw in keyword_pubs: keyword_pubs[kw].append(html)
+        else: keyword_pubs[kw] = [html]
+
+    keyword_html = ", ".join('<a target="_blank" href="keywords/{kw}.html">{kw}</a>'.format(kw=kw) for kw in keywords)
+    html = HTML.format(url = entry['link'], title=entry['title'],
+                       authors = authors, journal = journal,
+                       year = entry['year'], abstract=entry['abstract'],
+                       bibdata=raw_bibdata, keywords = keyword_html)
+    return html
 
 def library_to_html(lib):
     pubs_per_year = {}
@@ -52,16 +83,25 @@ def library_to_html(lib):
     for y in sorted(pubs_per_year.keys(),reverse=True):
         pubs = pubs_per_year[y]
         output += "<h2>{:d}</h2>\n".format(2000+y)
-        output += "<ul>\n"
+        output += '<ul>\n'
         for e in [entry_to_html(b) for b in sorted(pubs,key=entry_sort_key,reverse=True)]:
-            output += "<li>" + e + "</li>" +"\n"
+            output += '<li class="pub_entry">' + e + "</li>" +"\n"
         output += "</ul>\n \n"
-    #entries = [entry_to_html(b) for b in sorted(lib.entries,key=entry_sort_key,reverse=True)]
-    #output = ""
-    #for e in entries:
-    #    output += "<li>" + e + "</li>" +"\n"
 
     return output
+
+def generate_keyword_page(kw):
+    entries = keyword_pubs[kw]
+    output = "<ul>\n"
+    for e in entries:
+        output += '<li class="pub_entry">' + e + "</li>" +"\n"
+    output += "</ul>\n \n"
+    return output
+
+def generate_keyword_html():
+    l = list(keyword_pubs.keys())
+    l.sort(key=lambda v: len(keyword_pubs[v]),reverse=True)
+    return ", ".join('<a target="_blank" href="keywords/{kw}.html">{kw}</a> ({n:d})'.format(kw=kw,n=len(keyword_pubs[kw])) for kw in l)
 
 def to_clipboard(s):
     try:
@@ -79,11 +119,21 @@ if __name__ == '__main__':
     with open('zx-papers.bib',encoding='utf-8') as bibtex_file:
         bib_database = bibtexparser.load(bibtex_file)
         bib_data = library_to_html(bib_database)
-    with open('html/base_html.html') as f:
-        html_base = f.read()
     with open('html/expand.js') as f:
         js_base = f.read()
-    output = html_base.format(content=bib_data,javascript=js_base)
+    #Generate main page
+    with open('html/base_html.html') as f:
+        html_base = f.read()
+    output = html_base.format(keywords=generate_keyword_html(), content=bib_data,javascript=js_base)
     f = open("publications.html",'wb')
     f.write(output.encode('utf-8'))
     f.close()
+    #Generate keyword pages
+    with open('html/keyword_page.html') as f:
+        html_base = f.read()
+    for kw in keyword_pubs:
+        content = generate_keyword_page(kw)
+        output = html_base.format(keyword=kw,content=content,javascript=js_base)
+        f = open("keywords/{}.html".format(kw.lower()),'wb')
+        f.write(output.encode('utf-8'))
+        f.close()
--- html/expand.js
+++ html/expand.js
@@ -1,36 +1,56 @@
 jQuery(document).ready(function ($) {
 
-    $('p.abstract').each(function(event){ /* select all divs with the item class */
-    
-        var max_length = 50; /* set the max content length before a read more link will be added */
-        
-        if($(this).html().length > max_length){ /* check for content length */
-            
-            var short_content   = $(this).html().substr(0,max_length) +'... '; /* split the content in two parts */
-            var long_content    = $(this).html();
-            
-            $(this).html('<span class="short_text">' + short_content + '</span>' +
-                         '<a href="#" class="read_more">More</a>'+
-                         '<span class="more_text" style="display:none;">'+long_content+'</span>'+ '&nbsp;&nbsp;' +
-                         '<a href="#" class="read_less" style="display:none;">Less</a>'); /* Alter the html to allow the read more functionality */
-                         
-            $(this).find('a.read_more').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
- 
-                event.preventDefault(); /* prevent the a from changing the url */
-                $(this).hide(); /* hide the read more button */
-                $(this).parents('p.abstract').find('.short_text').hide();
-                $(this).parents('p.abstract').find('.more_text').show(); /* show the .more_text span */
-                $(this).parents('p.abstract').find('a.read_less').show();
-            });
+    $('span.abstract').each(function(event){ /* select all divs with the item class */
 
-            $(this).find('a.read_less').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
- 
-                event.preventDefault(); /* prevent the a from changing the url */
-                $(this).hide(); /* hide the read more button */
-                $(this).parents('p.abstract').find('.short_text').show();
-                $(this).parents('p.abstract').find('.more_text').hide(); /* show the .more_text span */
-                $(this).parents('p.abstract').find('.read_more').show();
-            });
-        }
+        var abstract = $(this).html();
+        
+        $(this).html('<a href="#" class="abstract_more">Abstract</a>' + '&nbsp;&nbsp;' + 
+                     '<span class="abstract_text" style="display:none;">'+ abstract + '</span>'+ '&nbsp;&nbsp;' +
+                     '<a href="#" class="read_less" style="display:none;">Collapse</a>'); /* Alter the html to allow the read more functionality */
+                     
+        $(this).find('a.abstract_more').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
+
+            event.preventDefault(); /* prevent the a from changing the url */
+            /*$(this).hide(); */ /* hide the read more button */
+            /*$(this).parents('span.abstract').find('.short_text').hide();*/
+            $(this).parents('span.abstract').find('.abstract_text').show(); /* show the .more_text span */
+            $(this).parents('span.abstract').find('a.read_less').show();
+        });
+
+        $(this).find('a.read_less').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
+
+            event.preventDefault(); /* prevent the a from changing the url */
+            $(this).hide(); /* hide the read more button */
+            /*$(this).parents('span.abstract').find('.short_text').show();*/
+            $(this).parents('span.abstract').find('.abstract_text').hide(); /* show the .more_text span */
+            $(this).parents('span.abstract').find('.abstract_more').show();
+        });
+    });
+
+    $('span.bibdata').each(function(event){ /* select all divs with the item class */
+
+        var bibdata = $(this).html();
+        
+        $(this).html('<a href="#" class="bibdata_more">Bibdata</a>' + '&nbsp;&nbsp;' + 
+                     '<span class="bibdata_text" style="display:none;">'+ bibdata + '</span>'+ '&nbsp;&nbsp;' +
+                     '<a href="#" class="read_less" style="display:none;">Collapse</a>'); /* Alter the html to allow the read more functionality */
+                     
+        $(this).find('a.bibdata_more').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
+
+            event.preventDefault(); /* prevent the a from changing the url */
+            /*$(this).hide(); */ /* hide the read more button */
+            /*$(this).parents('span.abstract').find('.short_text').hide();*/
+            $(this).parents('span.bibdata').find('.bibdata_text').show(); /* show the .more_text span */
+            $(this).parents('span.bibdata').find('a.read_less').show();
+        });
+
+        $(this).find('a.read_less').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
+
+            event.preventDefault(); /* prevent the a from changing the url */
+            $(this).hide(); /* hide the read more button */
+            /*$(this).parents('span.abstract').find('.short_text').show();*/
+            $(this).parents('span.bibdata').find('.bibdata_text').hide(); /* show the .more_text span */
+            $(this).parents('span.bibdata').find('.bibdata_more').show();
+        });
     });
 });
