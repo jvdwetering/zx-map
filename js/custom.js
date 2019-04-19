e585d67fadff4f51ddd017c8b6166bdf202e7be7
John van de Wetering <wetering@cs.ru.nl>
2019-04-19 15:51:40 +0200

    Added PyZX Demo page

--- js/custom.js
+++ js/custom.js
@@ -81,22 +81,22 @@ jQuery(document).ready(function($) {
                        });
 
 
-// Google Site Search | Search box
-(function() {
- var cx = '012777421281883581498:r3xp4rmwevw';
- var gcse = document.createElement('script');
- gcse.type = 'text/javascript';
- gcse.async = true;
- gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
- '//cse.google.com/cse.js?cx=' + cx;
- var s = document.getElementsByTagName('script')[0];
- s.parentNode.insertBefore(gcse, s);
- })();
+// // Google Site Search | Search box
+// (function() {
+//  var cx = '012777421281883581498:r3xp4rmwevw';
+//  var gcse = document.createElement('script');
+//  gcse.type = 'text/javascript';
+//  gcse.async = true;
+//  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+//  '//cse.google.com/cse.js?cx=' + cx;
+//  var s = document.getElementsByTagName('script')[0];
+//  s.parentNode.insertBefore(gcse, s);
+//  })();
 
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
- (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
- m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-70658068-1', 'auto');
-ga('send', 'pageview');
+// // Google Analytics
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+// ga('create', 'UA-70658068-1', 'auto');
+// ga('send', 'pageview');
