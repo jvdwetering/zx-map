582cd443f9439c95d0bbb3003b3b0e78b3721e08
John van de Wetering <wetering@cs.ru.nl>
2019-07-30 17:09:29 +0200

    Try fix for mobile for publications page

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,28 @@
 
-function sizing(windowWidth) {
-    //alert(windowWidth);
+// function sizing(windowWidth) {
+//     //alert(windowWidth);
 
-    if(windowWidth <= 480){ // ipad:768, Nexus10:800, 480
-        $('.allshow').hide(); //
-        $('.noshow').show();
-        $('.expandshow').show();
-        $('.collapseshow').hide();
-    } else {
-        $('.allshow').show();
-        $('.noshow').hide();
-        $('.expandshow').hide();
-        $('.collapseshow').hide();
-    }
-}
+//     if(windowWidth <= 480){ // ipad:768, Nexus10:800, 480
+//         $('.allshow').hide(); //
+//         $('.noshow').show();
+//         $('.expandshow').show();
+//         $('.collapseshow').hide();
+//     } else {
+//         $('.allshow').show();
+//         $('.noshow').hide();
+//         $('.expandshow').hide();
+//         $('.collapseshow').hide();
+//     }
+// }
 
-jQuery(document).ready(function($) {
-                       var windowWidth = $(window).width();
-                       $(window).resize(function(){
-                                        // Check window width has actually changed and it's not just iOS triggering a resize event on scroll
-                                        if ($(window).width() != windowWidth) {
-                                        windowWidth = $(window).width();
-                                        sizing(windowWidth);
-                                        }
-                                        });
-                       sizing(windowWidth);
-                       });
+// jQuery(document).ready(function($) {
+//                        var windowWidth = $(window).width();
+//                        $(window).resize(function(){
+//                                         // Check window width has actually changed and it's not just iOS triggering a resize event on scroll
+//                                         if ($(window).width() != windowWidth) {
+//                                         windowWidth = $(window).width();
+//                                         sizing(windowWidth);
+//                                         }
+//                                         });
+//                        sizing(windowWidth);
+//                        });
